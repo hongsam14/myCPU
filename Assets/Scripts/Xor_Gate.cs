e84fab7a61800c460d8614689ebc25ea741b8af8
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2021-10-01 16:55:02 +0900

    add xor or not gate

---
+++ Assets/Scripts/Xor_Gate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xor_Gate : GateBehaviour
+{
+    protected override void Runtime()
+    {
+        base.Runtime();
+        bool value = input_val(0) ^ input_val(1);
+        output_val(0, value);
+        Switch_Color(value);
+    }
+
+    private void Switch_Color(bool bit)
+    {
+        switch (bit)
+        {
+            case true:
+                color = Color.yellow;
+                break;
+            case false:
+                color = Color.grey;
+                break;
+        }
+    }
+}
