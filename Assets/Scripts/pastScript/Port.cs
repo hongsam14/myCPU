18644620c53ada0aa602ea308743c32e4c18d2ab
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2022-02-26 20:16:01 +0900

    end

---
+++ Assets/Scripts/pastScript/Port.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using System;
+
+namespace BaseModel
+{
+    [Serializable]
+    public class Port
+    {
+        public int index { get; private set; }
+        public bool Bit { get; set; } = false;
+        public Dir type { get; private set; }
+        public GameObject obj { get; private set; }
+
+        public Port From
+        {
+            get => _from;
+            private set
+            {
+                _from = value;
+            }
+        }
+        public Port To
+        {
+            get => _to;
+            private set
+            {
+                _to = value;
+            }
+        }
+
+        private Port _from;
+        private Port _to;
+
+        public void ConnectTo(Port to)
+        {
+            this.To = to;
+            to.From = this;
+        }
+
+        public void ConnectFrom(Port from)
+        {
+            this.From = from;
+            from.To = this;
+        }
+
+        public void Disconnect()
+        {
+            if (type == Dir.exit)
+                this.To = null;
+            else
+                this.From = null;
+        }
+
+        public void Update()
+        {
+            switch (type)
+            {
+                case Dir.enter:
+                    if (_from != null)
+                        this.Bit = _from.Bit;
+                    break;
+                case Dir.exit:
+                    if (_to != null)
+                        To.Bit = this.Bit;
+                    break;
+                case Dir.sel:
+                    if (_from != null)
+                        this.Bit = _from.Bit;
+                    break;
+            }
+        }
+
+        public Port Connected()
+        {
+            switch (type)
+            {
+                case Dir.enter:
+                    return this._from;
+                case Dir.exit:
+                    return this._to;
+                case Dir.sel:
+                    return this._from;
+                default:
+                    return null;
+            }
+        }
+
+        public override string ToString()
+        {
+            string name = "";
+            //return base.ToString();
+            switch (type)
+            {
+                case Dir.enter:
+                    name = "in";
+                    break;
+                case Dir.exit:
+                    name = "out";
+                    break;
+                case Dir.sel:
+                    name = "sel";
+                    break;
+            }
+            return (name + String.Format("[{0}]", this.index));
+        }
+
+        public Port(GameObject obj, int index, Dir option)
+        {
+            this.obj = obj;
+            this.index = index;
+            type = option;
+            switch (option)
+            {
+                case Dir.enter:
+                    _to = this;
+                    _from = null;
+                    break;
+                case Dir.exit:
+                    _from = this;
+                    _to = null;
+                    break;
+                case Dir.sel:
+                    _to = this;
+                    _from = null;
+                    break;
+            }
+        }
+
+        [Flags]
+        public enum Dir
+        {
+            enter,
+            exit,
+            sel
+        }
+    }
+}
